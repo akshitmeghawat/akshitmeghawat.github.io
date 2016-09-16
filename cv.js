0c4fa692ecf0e8499a87f2cd537d16e09d572cd0
Akshit Meghawat <meghawat.akshit@gmail.com>
2016-09-16 15:39:19 +0530

    making of cv div

--- cv.js
+++ cv.js
@@ -1,3 +1,20 @@
 app.controller('cvCtrl', function($scope){
-  $scope.cvMessage = "hello";
+  $scope.contactInfo = {
+    email: "meghawat.akshit@gmail.com",
+    number: "8870532436"
+  };
+  $scope.name = "Akshit Meghawat";
+  $scope.data = [
+    {
+      sectionName: "Professional Experience",
+      subSections: [
+        {
+          position: "Junior Digital Analyst"
+        }
+      ]
+    }
+  ];
+  $scope.downloadCV = function(){
+    console.log("yo");
+  };
 });
