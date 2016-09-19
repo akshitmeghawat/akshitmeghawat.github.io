bc90a7d3382f87f8b2d62e7bbd673d6062a6faee
Akshit Meghawat <meghawat.akshit@gmail.com>
2016-09-19 17:38:48 +0530

    basic home and education data

--- home/home.js
+++ home/home.js
@@ -1,3 +1,30 @@
 app.controller('homeCtrl', function($scope){
-  $scope.message = "Hello Akshit Meghawat";
+  $scope.whoAmI = ["A software developer", "An athlete", "An adventurous traveller"];
+  $scope.baseImgUrl = "home/images/";
+  $scope.career = [{
+    logoHref: "McKinsey_&_Company_logo.png",
+    name: "McKinsey & Company",
+    time: "January 2016 - present",
+    position: "Junior Digital Analyst",
+    description: []
+  }, {
+    logoHref: "McKinsey_&_Company_logo.png",
+    name: "McKinsey & Company",
+    time: "January 2015 - December 2015",
+    position: "Software Developer, Intern",
+    description: []
+  }, {
+    logoHref: "trusted.png",
+    name: "Trusted Shares & Invst Ltd",
+    time: "January 2015 - December 2015",
+    position: "Software Developer, Intern",
+    description: []
+  }];
+  $scope.education = [{
+    logoHref: "vitlogo.png",
+    name: "VIT University",
+    time: "July 2011 - May 2015",
+    position: "B. Tech. Information Technology",
+    description: []
+  }];
 });
